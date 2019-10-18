a2a4244a960a1d3f4769b75db4c5067c0140ff89
cyliu <chris.liu.hk@icloud.com>
2019-10-18 14:03:08 +0800

    framework go

---
+++ yourCode/compile.sh
@@ -0,0 +1,12 @@
+BASE_PATH=$(cd `dirname $0` && pwd)
+BIN_PATH=$BASE_PATH/../bin
+
+# Build the student's raft node implementation.
+# Exit immediately if there was a compile-time erro
+go build -o $BIN_PATH/raftrunner $BASE_PATH
+if [ $? -ne 0 ]; then
+   echo "FAIL: code does not compile"
+   exit $?
+fi
+
+
---
+++ yourCode/main.go
@@ -0,0 +1,206 @@
+package main
+
+import (
+	"context"
+	"csci4160/asgn2/raft"
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	ports := os.Args[2]
+	myport, _ := strconv.Atoi(os.Args[1])
+	nodeID, _ := strconv.Atoi(os.Args[3])
+	heartBeatInterval, _ := strconv.Atoi(os.Args[4])
+	electionTimeout, _ := strconv.Atoi(os.Args[5])
+
+	portStrings := strings.Split(ports, ",")
+
+	// A map where
+	// 		the key is the node id
+	//		the value is the {hostname:port}
+	nodeidPortMap := make(map[int]int)
+	for i, portStr := range portStrings {
+		port, _ := strconv.Atoi(portStr)
+		nodeidPortMap[i] = port
+	}
+
+	// Create and start the Raft Node.
+	_, err := NewRaftNode(myport, nodeidPortMap,
+		nodeID, heartBeatInterval, electionTimeout)
+
+	if err != nil {
+		log.Fatalln("Failed to create raft node:", err)
+	}
+
+	// Run the raft node forever.
+	select {}
+}
+
+type raftNode struct {
+	log []*raft.LogEntry
+	// TODO: Implement this!
+
+}
+
+// Desc:
+// NewRaftNode creates a new RaftNode. This function should return only when
+// all nodes have joined the ring, and should return a non-nil error if this node
+// could not be started in spite of dialing any other nodes.
+//
+// Params:
+// myport: the port of this new node. We use tcp in this project.
+//			   	Note: Please listen to this port rather than nodeidPortMap[nodeId]
+// nodeidPortMap: a map from all node IDs to their ports.
+// nodeId: the id of this node
+// heartBeatInterval: the Heart Beat Interval when this node becomes leader. In millisecond.
+// electionTimeout: The election timeout for this node. In millisecond.
+func NewRaftNode(myport int, nodeidPortMap map[int]int, nodeId, heartBeatInterval,
+	electionTimeout int) (raft.RaftNodeServer, error) {
+	// TODO: Implement this!
+
+	//remove myself in the hostmap
+	delete(nodeidPortMap, nodeId)
+
+	//a map for {node id, gRPCClient}
+	hostConnectionMap := make(map[int32]raft.RaftNodeClient)
+
+	rn := raftNode{
+		log: nil,
+	}
+
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", myport))
+
+	if err != nil {
+		log.Println("Fail to listen port", err)
+		os.Exit(1)
+	}
+
+	s := grpc.NewServer()
+	raft.RegisterRaftNodeServer(s, &rn)
+
+	log.Printf("Start listening to port: %d", myport)
+	go s.Serve(l)
+
+	//Try to connect nodes
+	for tmpHostId, hostPorts := range nodeidPortMap {
+		hostId := int32(tmpHostId)
+		numTry := 0
+		for {
+			numTry++
+
+			conn, err := grpc.Dial(fmt.Sprintf("127.0.0.1:%d", hostPorts), grpc.WithInsecure(), grpc.WithBlock())
+			//defer conn.Close()
+			client := raft.NewRaftNodeClient(conn)
+			if err != nil {
+				log.Println("Fail to connect other nodes. ", err)
+				time.Sleep(1 * time.Second)
+			} else {
+				hostConnectionMap[hostId] = client
+				break
+			}
+		}
+	}
+	log.Printf("Successfully connect all nodes")
+
+	//TODO: kick off leader election here !
+
+	return &rn, nil
+}
+
+// Desc:
+// Propose initializes proposing a new operation, and replies with the
+// result of committing this operation. Propose should not return until
+// this operation has been committed, or this node is not leader now.
+//
+// If the we put a new <k, v> pair or deleted an existing <k, v> pair
+// successfully, it should return OK; If it tries to delete an non-existing
+// key, a KeyNotFound should be returned; If this node is not leader now,
+// it should return WrongNode as well as the currentLeader id.
+//
+// Params:
+// args: the operation to propose
+// reply: as specified in Desc
+func (rn *raftNode) Propose(ctx context.Context, args *raft.ProposeArgs) (*raft.ProposeReply, error) {
+	// TODO: Implement this!
+	log.Printf("Receive propose from client")
+	var ret raft.ProposeReply
+
+	return &ret, nil
+}
+
+// Desc:GetValue
+// GetValue looks up the value for a key, and replies with the value or with
+// the Status KeyNotFound.
+//
+// Params:
+// args: the key to check
+// reply: the value and status for this lookup of the given key
+func (rn *raftNode) GetValue(ctx context.Context, args *raft.GetValueArgs) (*raft.GetValueReply, error) {
+	// TODO: Implement this!
+	var ret raft.GetValueReply
+	return &ret, nil
+}
+
+// Desc:
+// Receive a RecvRequestVote message from another Raft Node. Check the paper for more details.
+//
+// Params:
+// args: the RequestVote Message, you must include From(src node id) and To(dst node id) when
+// you call this API
+// reply: the RequestVote Reply Message
+func (rn *raftNode) RequestVote(ctx context.Context, args *raft.RequestVoteArgs) (*raft.RequestVoteReply, error) {
+	// TODO: Implement this!
+	var reply raft.RequestVoteReply
+	return &reply, nil
+}
+
+// Desc:
+// Receive a RecvAppendEntries message from another Raft Node. Check the paper for more details.
+//
+// Params:
+// args: the AppendEntries Message, you must include From(src node id) and To(dst node id) when
+// you call this API
+// reply: the AppendEntries Reply Message
+func (rn *raftNode) AppendEntries(ctx context.Context, args *raft.AppendEntriesArgs) (*raft.AppendEntriesReply, error) {
+	// TODO: Implement this
+	var reply raft.AppendEntriesReply
+	return &reply, nil
+}
+
+// Desc:
+// Set electionTimeOut as args.Timeout milliseconds.
+// You also need to stop current ticker and reset it to fire every args.Timeout milliseconds.
+//
+// Params:
+// args: the heartbeat duration
+// reply: no use
+func (rn *raftNode) SetElectionTimeout(ctx context.Context, args *raft.SetElectionTimeoutArgs) (*raft.SetElectionTimeoutReply, error) {
+	// TODO: Implement this!
+	var reply raft.SetElectionTimeoutReply
+	return &reply, nil
+}
+
+// Desc:
+// Set heartBeatInterval as args.Interval milliseconds.
+// You also need to stop current ticker and reset it to fire every args.Interval milliseconds.
+//
+// Params:
+// args: the heartbeat duration
+// reply: no use
+func (rn *raftNode) SetHeartBeatInterval(ctx context.Context, args *raft.SetHeartBeatIntervalArgs) (*raft.SetHeartBeatIntervalReply, error) {
+	// TODO: Implement this!
+	var reply raft.SetHeartBeatIntervalReply
+	return &reply, nil
+}
+
+//NO NEED TO TOUCH THIS FUNCTION
+func (rn *raftNode) CheckEvents(context.Context, *raft.CheckEventsArgs) (*raft.CheckEventsReply, error) {
+	return nil, nil
+}
