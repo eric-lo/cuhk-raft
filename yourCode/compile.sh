1d160f587c54448cae34430a3fec215c8dad260a
Chenxia Han <cxhan@cse.cuhk.edu.hk>
2022-01-14 01:53:12 +0800

    Tidy dependencies

--- yourCode/compile.sh
+++ yourCode/compile.sh
@@ -3,6 +3,7 @@ BIN_PATH=$BASE_PATH/../bin
 
 # Build the student's raft node implementation.
 # Exit immediately if there was a compile-time erro
+go mod tidy
 go build -o $BIN_PATH/raftrunner $BASE_PATH
 if [ $? -ne 0 ]; then
    echo "FAIL: code does not compile"
